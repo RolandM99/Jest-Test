adf3b52e912e0d996a948b789ddfd441db660ca7
rolandm99 <manfulmweze99@gmail.com>
2021-11-16 09:38:02 +0100

    Add source file

---
+++ src/functionality.test.js
@@ -0,0 +1,37 @@
+// eslint-disable-next-line import/no-unresolved, import/no-extraneous-dependencies
+import { JSDOM } from 'jsdom';
+import Tasks from './task.js';
+
+const dom = new JSDOM(`<!DOCTYPE html><body><ul id="addList"></body>`); // eslint-disable-line
+
+global.document = dom.window.document;
+global.window = dom.window;
+
+const createList = document.getElementById('addList');
+const elementToAdd = {
+  task: 'Graduate from Microverse',
+  completed: false,
+  index: 1,
+};
+
+Tasks.myTasksView(elementToAdd, createList);
+
+test("Add exactly one 'li' element in the list", () => {
+  expect(createList.childElementCount).toBe(1);
+});
+
+Tasks.addTask(elementToAdd);
+describe('Test remove an add methods in the Tasks class', () => {
+  test('Add elements properties in the LocalStorage', () => {
+    expect(Tasks.getFromLocalStore()[0].task).toBe('Graduate from Microverse');
+  });
+  test('Remove element from the DOM and from LocalStorage', () => {
+    const target = document.getElementById('1 trash');
+    const childcount = createList.childElementCount;
+    Tasks.deleteTask(target, 0);
+    expect(createList.childElementCount).toBeLessThan(childcount);
+  });
+  test('Remove element from DOM and from LocalStorage', () => {
+    expect(Tasks.getFromLocalStore()).toBeDefined();
+  });
+});
